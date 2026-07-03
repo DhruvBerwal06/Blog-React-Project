5abc343374ec352a3a158be0d041079bfe32fc8e
DhruvBerwal06 <dhruvberwal06@gmail.com>
2026-07-03 23:31:31 +0530

    Revert "made changes so the editor is accessible from mobile as well"
    This reverts commit 0d70bdb8b2bf658f0a510951881bc6ecd6ba3c2d.

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -2,29 +2,6 @@ import React from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import { Controller } from "react-hook-form";
 
-const tinyMcePlugins = [
-  "advlist",
-  "autolink",
-  "lists",
-  "link",
-  "image",
-  "charmap",
-  "preview",
-  "anchor",
-  "searchreplace",
-  "visualblocks",
-  "code",
-  "fullscreen",
-  "insertdatetime",
-  "media",
-  "table",
-  "help",
-  "wordcount",
-];
-
-const tinyMceToolbar =
-  "undo redo | blocks | bold italic underline forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media | removeformat | help";
-
 export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
     <div className="w-full">
@@ -33,41 +10,40 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
       <Controller
         name={name || "content"}
         control={control}
-        render={({ field: { onChange, value } }) => (
+        render={({ field: { onChange } }) => (
           <Editor
             apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
-            value={value || defaultValue}
             initialValue={defaultValue}
             init={{
-              height: 420,
-              min_height: 280,
-              menubar: false,
-              plugins: tinyMcePlugins,
-              toolbar: tinyMceToolbar,
-              toolbar_mode: "floating",
-              mobile: {
-                menubar: true,
-                toolbar_mode: "scrolling",
-                toolbar: [
-                  "undo",
-                  "redo",
-                  "bold",
-                  "italic",
-                  "link",
-                  "image",
-                  "bullist",
-                  "numlist",
-                  "outdent",
-                  "indent",
-                  "removeformat",
-                ],
-              },
+              initialValue: defaultValue,
+              height: 500,
+              menubar: true,
+              plugins: [
+                "image",
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "code",
+                "help",
+                "wordcount",
+                "anchor",
+              ],
+              toolbar:
+                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
               content_style:
-                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px; line-height:1.5; }",
-              browser_spellcheck: true,
-              paste_data_images: true,
-              forced_root_block: "p",
-              resize: false,
+                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
             }}
             onEditorChange={onChange}
           />
