0d70bdb8b2bf658f0a510951881bc6ecd6ba3c2d
DhruvBerwal06 <dhruvberwal06@gmail.com>
2026-07-03 23:28:25 +0530

    made changes so the editor is accessible from mobile as well

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -2,6 +2,29 @@ import React from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import { Controller } from "react-hook-form";
 
+const tinyMcePlugins = [
+  "advlist",
+  "autolink",
+  "lists",
+  "link",
+  "image",
+  "charmap",
+  "preview",
+  "anchor",
+  "searchreplace",
+  "visualblocks",
+  "code",
+  "fullscreen",
+  "insertdatetime",
+  "media",
+  "table",
+  "help",
+  "wordcount",
+];
+
+const tinyMceToolbar =
+  "undo redo | blocks | bold italic underline forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media | removeformat | help";
+
 export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
     <div className="w-full">
@@ -10,40 +33,41 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
       <Controller
         name={name || "content"}
         control={control}
-        render={({ field: { onChange } }) => (
+        render={({ field: { onChange, value } }) => (
           <Editor
             apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
+            value={value || defaultValue}
             initialValue={defaultValue}
             init={{
-              initialValue: defaultValue,
-              height: 500,
-              menubar: true,
-              plugins: [
-                "image",
-                "advlist",
-                "autolink",
-                "lists",
-                "link",
-                "image",
-                "charmap",
-                "preview",
-                "anchor",
-                "searchreplace",
-                "visualblocks",
-                "code",
-                "fullscreen",
-                "insertdatetime",
-                "media",
-                "table",
-                "code",
-                "help",
-                "wordcount",
-                "anchor",
-              ],
-              toolbar:
-                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+              height: 420,
+              min_height: 280,
+              menubar: false,
+              plugins: tinyMcePlugins,
+              toolbar: tinyMceToolbar,
+              toolbar_mode: "floating",
+              mobile: {
+                menubar: true,
+                toolbar_mode: "scrolling",
+                toolbar: [
+                  "undo",
+                  "redo",
+                  "bold",
+                  "italic",
+                  "link",
+                  "image",
+                  "bullist",
+                  "numlist",
+                  "outdent",
+                  "indent",
+                  "removeformat",
+                ],
+              },
               content_style:
-                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px; line-height:1.5; }",
+              browser_spellcheck: true,
+              paste_data_images: true,
+              forced_root_block: "p",
+              resize: false,
             }}
             onEditorChange={onChange}
           />
